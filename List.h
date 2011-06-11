99c5e5185ff674aac5153dc3a5ab6265768d9c79
Scott Jackson <daneren2005@yahoo.com>
2011-06-10 17:20:03 -0700

    Added GPL license to all code

--- List.h
+++ List.h
@@ -1,9 +1,19 @@
 /*
- * File:   List.h
- * Author: scott
- *
- * Created on July 7, 2010, 7:17 PM
- */
+	This file is part of Skyfire.
+
+    Skyfire is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Skyfire is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Skyfire.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _LIST_H
 #define	_LIST_H
