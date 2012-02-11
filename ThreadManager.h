48af6e721bc2396a3c1e73f7b0733e0c0f029dd9
Scott Jackson <daneren2005@gmail.com>
2012-02-10 17:46:19 -0800

    Changed ThreadManager to template to allow specialized job managers

--- ThreadManager.h
+++ ThreadManager.h
@@ -28,14 +28,15 @@
 #include "Array.h"
 #include "Queue.h"
 
-class ThreadManager
+template <class Param>
+class ThreadManagerBase
 {
 public:
-	ThreadManager();
-	virtual ~ThreadManager();
+	ThreadManagerBase();
+	virtual ~ThreadManagerBase();
 
-	void addJob(const Function<void, GenericType>& job, GenericType arg);
-	void addBulkJob(const Function<void, GenericType>& job, Array<GenericType> args);
+	void addJob(const Function<void, Param>& job, Param arg);
+	void addBulkJob(const Function<void, Param>& job, Array<Param> args);
 	void waitTillDone();
 	
 	void setWorkers(ulong workers);
@@ -43,12 +44,12 @@ public:
 private:
 	struct Job
 	{
-		Function<void, GenericType> job;
-		GenericType arg;
+		Function<void, Param> job;
+		Param arg;
 	};
 	struct Worker
 	{
-		ThreadManager* manager;
+		ThreadManagerBase* manager;
 		pthread_t thread;
 		bool running;
 	};
@@ -57,11 +58,137 @@ private:
 	Queue<Job*> jobs;
 
 	static void* workerFunction(void* arg);
-	static void deleteWorker(GenericType arg);
+	static void deleteWorker(Param arg);
 
 	// Don't allow use of
-	ThreadManager(ThreadManager& orig);
-	ThreadManager& operator=(ThreadManager& rhs);
+	ThreadManagerBase(ThreadManagerBase& orig);
+	ThreadManagerBase& operator=(ThreadManagerBase& rhs);
 };
+
+typedef ThreadManagerBase<GenericType> ThreadManager;
+
+template <class Param>
+ThreadManagerBase<Param>::ThreadManagerBase()
+{
+	this->setWorkers(1);
+}
+template <class Param>
+ThreadManagerBase<Param>::ThreadManagerBase(ThreadManagerBase& orig)
+{
+	// TODO: needs new set of workers + copied jobs
+	this->workers = orig.workers;
+	this->jobs = orig.jobs;
+}
+template <class Param>
+ThreadManagerBase<Param>::~ThreadManagerBase()
+{
+	// Empty out job queue
+	while(jobs.size() > 0)
+	{
+		jobs.remove();
+	}
+	
+	// Tell each Worker its done
+	for(typename List<Worker*>::Iterator it = workers.begin(); !it; it++)
+	{
+		it.value()->running = false;
+	}
+	
+	// Add a null job to each thread so they stop waiting and end
+	for(ulong i = 0; i < workers.size(); i++)
+	{
+		Job* job = new Job;
+		job->job = ThreadManagerBase<Param>::deleteWorker;
+		job->arg = Param();
+		jobs.insert(job);
+	}
+	
+	// TODO: Spinlock = bad
+	// Wait until no more worker threads waiting for work
+	while(jobs.waitingThreads() > 0) {}
+}
+
+template <class Param>
+void ThreadManagerBase<Param>::addJob(const Function<void, Param>& job, Param arg)
+{
+	Job* newJob = new Job;
+	newJob->job = job;
+	newJob->arg = arg;
+
+	jobs.insert(newJob);
+}
+template <class Param>
+void ThreadManagerBase<Param>::addBulkJob(const Function<void, Param>& job, Array<Param> args)
+{
+	for(ulong i = 0; i < args.size(); i++)
+	{
+		Job* newJob = new Job;
+		newJob->job = job;
+		// newJob->arg = args[i];
+	
+		jobs.insert(newJob);
+	}
+}
+template <class Param>
+void ThreadManagerBase<Param>::waitTillDone()
+{
+	jobs.waitTillEmpty();
+	// TODO: Spinlock = bad
+	while(jobs.waitingThreads() < workers.size()) {}
+}
+
+template <class Param>
+void ThreadManagerBase<Param>::setWorkers(ulong workers)
+{
+	// Increase number of workers
+	while(workers > this->workers.size())
+	{
+		Worker* worker = new Worker;
+		worker->running = true;
+		worker->manager = this;
+		pthread_create(&worker->thread, 0x0, workerFunction, (void*)worker);
+		this->workers.pushBack(worker);
+	}
+	// Decrease number of workers
+	while(workers < this->workers.size())
+	{
+		Worker* worker = this->workers.popBack();
+		worker->running = false;
+	}
+}
+template <class Param>
+ulong ThreadManagerBase<Param>::getWorkers()
+{
+	return workers.size();
+}
+
+template <class Param>
+void* ThreadManagerBase<Param>::workerFunction(void* arg)
+{
+	Worker* worker = (Worker*)arg;
+
+	while(worker->running)
+	{
+		Job* job = worker->manager->jobs.removeWait();
+		job->job(job->arg);
+		delete job;
+	}
+
+	delete worker;
+	return 0x0;
+}
+
+template <class Param>
+void ThreadManagerBase<Param>::deleteWorker(Param)
+{
+	
+}
+
+template <class Param>
+ThreadManagerBase<Param>& ThreadManagerBase<Param>::operator=(ThreadManagerBase& rhs)
+{
+	this->workers = rhs.workers;
+	this->jobs = rhs.jobs;
+}
 
 #endif
